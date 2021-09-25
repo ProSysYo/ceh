8a0eb1fcc1c5e9a76a94e182a4dfbfac82510658
Maxim Ivanov <maksimivanov91@yandex.ru>
2021-09-25 17:02:49 +0300

    удалил сервер

--- server/models/Role.js
+++
@@ -1,9 +0,0 @@
-const {Schema, model} = require('mongoose')
-
-const roleSchema = new Schema({
-    value: {type: String, unique: true, default: 'USER'}
-})
-
-const Role = model('Role', roleSchema)
-
-module.exports = {Role}

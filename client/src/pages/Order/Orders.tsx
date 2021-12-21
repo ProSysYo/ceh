c17b316c85c3046aa8bb9f8ec12f3de4f63d46d3
Maxim Ivanov <maksimivanov91@yandex.ru>
2021-12-21 16:54:58 +0300

    start to work plane date create order

--- client/src/pages/Order/Orders.tsx
+++ client/src/pages/Order/Orders.tsx
@@ -1,8 +1,8 @@
 import React, { useState } from 'react'
-import { Button, Table, Spin } from 'antd';
+import { Button, Table, Spin, Tooltip, DatePicker } from 'antd';
 import styled from 'styled-components';
 import { format} from 'date-fns'
-import { FileAddOutlined, EditOutlined, DeleteOutlined, EyeOutlined } from '@ant-design/icons';
+import { FileAddOutlined, EditOutlined, DeleteOutlined, EyeOutlined, ScheduleOutlined } from '@ant-design/icons';
 
 import { useAppSelector } from '../../hooks/useAppSelector';
 import OrdersFilter from '../../components/OrdersFilter';
@@ -14,10 +14,13 @@ import ShowOrder from './ShowOrder';
 import { useAppDispatch } from '../../hooks/useAppDispatch';
 import { getOrder } from '../../store/actions/orderActions';
 import { orderActions } from '../../store/slices/orderSlice';
+import Modal from 'antd/lib/modal/Modal';
 
 const Orders: React.FC = () => { 
     const { orders, isLoading } = useAppSelector(state => state.order)
-    const [visible, setVisible] = useState(false)     
+    const [visible, setVisible] = useState(false)
+    const [visibleCalendar, setVisibleCalendar] = useState(false)      
+    const [selectedId, setSelectedId] = useState("")      
     
     const dispatch = useAppDispatch()
 
@@ -28,7 +31,25 @@ const Orders: React.FC = () => {
     const onClose = async () => {
         await dispatch(orderActions.rebootCurrentOrder())
         setVisible(false);
-    };   
+    }; 
+    
+    const showCalendar = (id: string) => {
+        setSelectedId(id)
+        setVisibleCalendar(true)        
+    }
+
+    const calendarCancel = () => {
+        setSelectedId("")
+        setVisibleCalendar(false)
+    }
+
+    const calendarOk = () => {
+
+    }
+
+    const onChangeDate = () => {
+        
+    }
     
     return (
         <>  
@@ -38,8 +59,9 @@ const Orders: React.FC = () => {
             
             <Container>
                 <TableContainer>
-                    <Table dataSource={orders} pagination={{ pageSize: 40 }} scroll={{ y: 680 }} size="small" rowKey="number">
+                    <Table dataSource={orders} pagination={{ pageSize: 40 }} scroll={{ y: 680 }} size="small" rowKey="number" loading={isLoading}>
                         <Column title="Номер" dataIndex="number" width={15} />
+                        <Column title="Номера дверей" dataIndex="" width={15} />
                         <Column title="Заказчик" dataIndex="customer" width={10} />
                         <Column title="Номер заказчика" dataIndex="numberCustomer" width={15} />
                         <Column title="Модель" dataIndex="model" width={10} />
@@ -57,10 +79,22 @@ const Orders: React.FC = () => {
                             title = 'Действия'                                  
                             width = {20} 
                             render = {(record: IOrder) => (
-                                <>
-                                    <ActionButton icon={ <EyeOutlined/> } onClick={() => showDrawer(record._id!)} size="small"/>
-                                    <ActionLink to={`/editorder/${record._id}`}><EditOutlined/></ActionLink>
-                                    <ActionButton icon={ <DeleteOutlined/> } size="small"/>                
+                                <> 
+                                    <Tooltip placement="bottom" title="Просмотр">
+                                        <ActionButton icon={ <EyeOutlined/> } onClick={() => showDrawer(record._id!)} size="small"/>
+                                    </Tooltip>
+
+                                    <Tooltip placement="bottom" title="Редактировать">
+                                        <ActionLink to={`/editorder/${record._id}`}><EditOutlined/></ActionLink>
+                                    </Tooltip>
+
+                                    <Tooltip placement="bottom" title="Удалить">
+                                        <ActionButton icon={ <DeleteOutlined/> } size="small"/>
+                                    </Tooltip>
+
+                                    <Tooltip placement="bottom" title="Запустить">
+                                        <ActionButton icon={ <ScheduleOutlined/> } onClick={() => showCalendar(record._id!)} size="small" />
+                                    </Tooltip>    
                                 </>
                             )}
                         />
@@ -70,6 +104,9 @@ const Orders: React.FC = () => {
                     <OrdersFilter/>
                 </FilterContainer>
                 <ShowOrder onClose={onClose} visible={visible}/>
+                <Modal title="Basic Modal" visible={visibleCalendar} onOk={calendarOk} onCancel={calendarCancel}>
+                    <DatePicker onChange={onChangeDate} />
+                </Modal>
             </Container>
             
             
